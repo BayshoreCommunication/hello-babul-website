9f6bd178d37bc3efb857bd8c5f689a89ea7717c4
sajolbd <shamim15-9760@diu.edu.bd>
2026-01-12 13:37:52 +0600

    login issue fix

--- app/api/auth/login/route.tsx
+++ app/api/auth/login/route.tsx
@@ -3,28 +3,36 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const { email, password } = await req.json();
 
-  const ADMIN_EMAIL = process.env.ADMIN_EMAIL || "shamim@gmail.com";
-  const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "bay@2026";
+  // .env থেকে email & password fetch
+  const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+  const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
+  if (!ADMIN_EMAIL || !ADMIN_PASSWORD) {
+    return NextResponse.json(
+      { success: false, message: "Server login config missing" },
+      { status: 500 }
+    );
+  }
+
+  // Validate login
   if (email !== ADMIN_EMAIL || password !== ADMIN_PASSWORD) {
     return NextResponse.json(
-      {
-        success: false,
-        message: "Invalid email or password",
-      },
+      { success: false, message: "Invalid email or password" },
       { status: 401 }
     );
   }
 
+  // Login success
   const response = NextResponse.json({
     success: true,
     message: "Login successful",
   });
 
+  // Cookie set
   response.cookies.set("auth_token", "secure_admin_token", {
     httpOnly: true,
     path: "/",
-    maxAge: 60 * 60 * 24,
+    maxAge: 60 * 60 * 24, // 1 day
   });
 
   return response;
